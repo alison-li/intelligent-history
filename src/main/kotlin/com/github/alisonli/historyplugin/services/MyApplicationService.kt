546442ab0221f4ff857f67fe78c7475c04885752
GitHub Action <action@github.com>
2022-03-22 20:28:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/alisonli/historyplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/alisonli/historyplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.alisonli.historyplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alisonli.historyplugin.MyBundle
 
 class MyApplicationService {
 
