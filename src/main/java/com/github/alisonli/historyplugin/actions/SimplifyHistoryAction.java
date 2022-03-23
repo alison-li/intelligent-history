3357710ad6a80af85ae3d4ca92efef8160ae759d
Alison Li <al260708761@gmail.com>
2022-03-23 15:50:29 -0700

    Add `update` implementations to actions

--- src/main/java/com/github/alisonli/historyplugin/actions/SimplifyHistoryAction.java
+++ src/main/java/com/github/alisonli/historyplugin/actions/SimplifyHistoryAction.java
@@ -1,21 +1,39 @@
 package com.github.alisonli.historyplugin.actions;
 
-import com.intellij.ide.BrowserUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
 public class SimplifyHistoryAction extends AnAction {
 
+    /**
+     * Enable action only when a project and a file from the project is currently open.
+     *
+     * @param e Carries information on the invocation place.
+     */
     @Override
     public void update(@NotNull AnActionEvent e) {
-        // TODO: Enable action only when file history tool window is open
+        Project project = e.getProject();
+        e.getPresentation().setEnabledAndVisible(project != null && isEnabled(e));
+    }
+
+    private boolean isEnabled(@NotNull AnActionEvent e) {
+        Editor editor = e.getData(CommonDataKeys.EDITOR);
+        if (editor == null) return false;
+        Document document = editor.getDocument();
+        VirtualFile virtualFile = FileDocumentManager.getInstance().getFile(document);
+        return virtualFile != null;
     }
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         // TODO: Implement
         // See: https://plugins.jetbrains.com/docs/intellij/basic-action-system.html
-        BrowserUtil.browse("https://github.com/Alison-Li/history-plugin");
     }
 }
