b0578e71dd20f934cdf2ed5e78c0713ba0ed5469
Alison Li <al260708761@gmail.com>
2022-03-29 17:02:00 -0700

    Add progress on obtaining revision info
    Able to retrieve `ContentRevision` now and can also obtain file content through them. Next is to decide how to proceed with heuristics application and consider refactoring the code into the service class.

--- src/main/java/com/github/alisonli/historyplugin/actions/SimplifyHistoryAction.java
+++ src/main/java/com/github/alisonli/historyplugin/actions/SimplifyHistoryAction.java
@@ -1,21 +1,30 @@
 package com.github.alisonli.historyplugin.actions;
 
+import com.github.alisonli.historyplugin.services.DiffAnalyzerService;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.*;
+import com.intellij.openapi.vcs.FilePath;
+import com.intellij.openapi.vcs.VcsDataKeys;
+import com.intellij.openapi.vcs.VcsException;
+import com.intellij.openapi.vcs.VcsKey;
+import com.intellij.openapi.vcs.changes.Change;
+import com.intellij.openapi.vcs.changes.ContentRevision;
 import com.intellij.openapi.vcs.history.VcsFileRevision;
-import com.intellij.openapi.vcs.history.VcsHistoryProvider;
-import com.intellij.openapi.vcs.history.VcsHistorySession;
 import com.intellij.vcs.log.VcsLogDataKeys;
 import com.intellij.vcs.log.VcsLogUi;
+import git4idea.GitFileRevision;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class SimplifyHistoryAction extends AnAction {
+
+    private static final Logger LOG = Logger.getInstance(SimplifyHistoryAction.class);
+
     /**
      * Enable action only when a project and a file from the project is currently open.
      *
@@ -34,25 +43,36 @@ public class SimplifyHistoryAction extends AnAction {
         VcsKey vcsKey = Objects.requireNonNull(e.getData(VcsDataKeys.VCS));
         FilePath filePath = Objects.requireNonNull(e.getData(VcsDataKeys.FILE_PATH));
 
-        List<VcsFileRevision> revisionList = getFileHistoryRevisionList(project, vcsKey, filePath);
-    }
+        DiffAnalyzerService diffAnalyzerService = new DiffAnalyzerService(project);
 
-    @Nullable
-    private static List<VcsFileRevision> getFileHistoryRevisionList(@NotNull Project project, @NotNull VcsKey vcsKey,
-                                                                    @NotNull FilePath filePath) {
-        AbstractVcs vcs = Objects.requireNonNull(getVcs(project, vcsKey));
-        VcsHistoryProvider historyProvider = Objects.requireNonNull(vcs.getVcsHistoryProvider());
-        try {
-            VcsHistorySession historySession = Objects.requireNonNull(historyProvider.createSessionFor(filePath));
-            return historySession.getRevisionList();
-        } catch (VcsException ex) {
-            ex.printStackTrace();
+        List<VcsFileRevision> revisionList = diffAnalyzerService.getFileHistoryRevisionList(vcsKey, filePath);
+        List<List<Change>> changesList = new ArrayList<>();
+        if (revisionList != null && revisionList.size() > 1) {
+            try {
+                for (int i = 0; i < revisionList.size() - 1; i++) {
+                    // Assume the VCS is git
+                    GitFileRevision rev1 = (GitFileRevision) revisionList.get(i);
+                    GitFileRevision rev2 = (GitFileRevision) revisionList.get(i + 1);
+                    List<Change> changes = diffAnalyzerService.getChangesBetweenRevisions(filePath, rev1, rev2);
+                    changesList.add(changes);
+                }
+            } catch (VcsException ex) {
+                ex.printStackTrace();
+            }
         }
-        return null;
-    }
 
-    @Nullable
-    private static AbstractVcs getVcs(@NotNull Project project, @Nullable VcsKey vcsKey) {
-        return vcsKey == null ? null : ProjectLevelVcsManager.getInstance(project).findVcsByName(vcsKey.getName());
+        for (List<Change> changes : changesList) {
+            Change change = changes.get(0);
+            Change.Type type = change.getType();
+            ContentRevision contentRevisionBefore = change.getBeforeRevision();
+            ContentRevision contentRevisionAfter = change.getAfterRevision();
+            try {
+                // TODO: Now we have retrieved the before and after as strings - do something with them.
+                String beforeContent = contentRevisionBefore.getContent();
+                String afterContent = contentRevisionAfter.getContent();
+            } catch (VcsException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
