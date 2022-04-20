bc45a58802dd282b478bb5671f0e8203bdfedc01
Alison Li <al260708761@gmail.com>
2022-04-20 11:35:18 -0700

    Refactor highlighter factory to use a cache

--- src/main/java/com/github/alisonli/historyplugin/highlighters/HighlighterFactory.java
+++ src/main/java/com/github/alisonli/historyplugin/highlighters/HighlighterFactory.java
@@ -2,34 +2,34 @@ package com.github.alisonli.historyplugin.highlighters;
 
 import com.github.alisonli.historyplugin.services.DiffAnalyzerService;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.FilePath;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.vcs.log.VcsLogHighlighter;
 import com.intellij.vcs.log.data.VcsLogData;
 import com.intellij.vcs.log.history.FileHistoryUi;
 import com.intellij.vcs.log.impl.VcsProjectLog;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
 public class HighlighterFactory {
-    private final Project myProject;
-    private final VirtualFile myRoot;
-    private final FileHistoryUi myUi;
+    private static final Map<FilePath, VcsLogHighlighter> HIGHLIGHTER_CACHE = new HashMap<>();
 
-    public HighlighterFactory(Project project, VirtualFile root, FileHistoryUi logUi) {
-        myProject = project;
-        myRoot = root;
-        myUi = logUi;
+    private HighlighterFactory() {
     }
 
-    public VcsLogHighlighter getHighlighter(Type type) {
-        // Can switch highlighter types based on input type when more highlighters are added
-        VcsLogData logData = Objects.requireNonNull(VcsProjectLog.getInstance(myProject).getLogManager()).getDataManager();
-        Set<Integer> importantCommits = DiffAnalyzerService.getImportantCommits(myProject, myRoot, myUi);
-        return ImportantCommitsHighlighter.getInstance(myUi, logData, myRoot, importantCommits);
+    public static VcsLogHighlighter getHighlighter(Project project, VirtualFile root, FilePath path,
+                                                   FileHistoryUi logUi) {
+        return HIGHLIGHTER_CACHE.computeIfAbsent(path, k -> {
+            VcsLogData logData = Objects.requireNonNull(VcsProjectLog.getInstance(project).getLogManager()).getDataManager();
+            Set<Integer> importantCommits = DiffAnalyzerService.getImportantCommits(project, root, logUi);
+            return new ImportantCommitsHighlighter(logUi, logData, root, importantCommits);
+        });
     }
 
-    public enum Type {
-        IMPORTANT
+    public static void disposeHighlighter(FilePath path) {
+        HIGHLIGHTER_CACHE.remove(path);
     }
 }
