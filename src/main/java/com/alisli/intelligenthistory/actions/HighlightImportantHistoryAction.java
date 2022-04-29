810e348f8646eb4abded58fd2d1802afd076d0b6
Alison Li <al260708761@gmail.com>
2022-04-29 14:30:08 -0700

    Fix bug in 2022.1 where highlighting toggle was not immediate

--- src/main/java/com/alisli/intelligenthistory/actions/HighlightImportantHistoryAction.java
+++ src/main/java/com/alisli/intelligenthistory/actions/HighlightImportantHistoryAction.java
@@ -45,5 +45,6 @@ public class HighlightImportantHistoryAction extends ToggleAction {
         } else {
             logUi.getTable().addHighlighter(highlighter);
         }
+        logUi.getMainComponent().updateUI();
     }
 }
