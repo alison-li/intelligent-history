ec357e734d1c79b7568bc5e8d5dfe3560b3e8642
Alison Li <al260708761@gmail.com>
2022-05-02 16:15:11 -0700

    Add external link to Jira issue

--- src/main/java/com/alisli/intelligenthistory/components/JiraIssuePanel.java
+++ src/main/java/com/alisli/intelligenthistory/components/JiraIssuePanel.java
@@ -3,12 +3,14 @@ package com.alisli.intelligenthistory.components;
 import com.alisli.intelligenthistory.IntelligentHistoryBundle;
 import com.intellij.ide.BrowserUtil;
 import com.intellij.openapi.Disposable;
+import com.intellij.ui.components.ActionLink;
+import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.util.ui.HTMLEditorKitBuilder;
 import com.intellij.util.ui.JBUI;
 import icons.MyIcons;
 
 import javax.swing.*;
-import javax.swing.event.HyperlinkEvent;
+
 
 public class JiraIssuePanel implements Disposable {
     private JPanel rootPanel;
@@ -18,7 +20,9 @@ public class JiraIssuePanel implements Disposable {
     public JiraIssuePanel() {
         rootPanel.setBorder(JBUI.Borders.empty(8));
         rootPanel.setFocusable(true);
+
         title.setText("");
+
         bodyText.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, true);
         bodyText.setEditorKit(new HTMLEditorKitBuilder().withWordWrapViewFactory().build());
         bodyText.setText(IntelligentHistoryBundle.message("ih.panel.startup.text"));
@@ -31,11 +35,17 @@ public class JiraIssuePanel implements Disposable {
 
     public void setBodyText(String bodyText) {
         this.bodyText.setText(bodyText);
-        this.bodyText.addHyperlinkListener(e -> {
-            if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-                BrowserUtil.open(String.valueOf(e.getURL()));
-            }
+    }
+
+    public void createActionLink(String url) {
+        ActionLink externalLink = new ActionLink(IntelligentHistoryBundle.message("ih.panel.external.jira.button.title"), e -> {
+            BrowserUtil.browse(url);
         });
+        externalLink.setExternalLinkIcon();
+        GridConstraints constraints = new GridConstraints();
+        constraints.setAnchor(GridConstraints.ANCHOR_SOUTHEAST);
+        constraints.setUseParentLayout(true);
+        rootPanel.add(externalLink, constraints);
     }
 
     public JPanel getContent() {
